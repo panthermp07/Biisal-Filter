60b9191e5ec0bf701779afb4b9ac561c351598e2
pantherop07 <139424054+pantherop07@users.noreply.github.com>
2024-04-17 07:54:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'radhe-radhe'))
 MULTI_CLIENT = False
-name = str(environ.get('name', 'sparrow-infinity'))
+name = str(environ.get('name', 'bisal-ki-bot'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
@@ -40,7 +40,7 @@ if HAS_SSL:
     URL = "https://{}/".format(FQDN)
 else:
     URL = "http://{}/".format(FQDN)
-REPO_OWNER = "InfinityBotz"
+REPO_OWNER = "Biisal"
 
 # Bot information
 SESSION = environ.get('SESSION', 'infinityfilter')
