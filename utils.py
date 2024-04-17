fb54cfbd7df98e85c7a3d3435415b57d100c6d67
pantherop07 <139424054+pantherop07@users.noreply.github.com>
2024-04-17 07:10:13 +0530

    Pantherop07 patch 1 (#2)
    * Update info.py
    
    * Update utils.py
    
    * Update app.json
    
    * Update requirements.txt
    
    * Update pm_filter.py

--- utils.py
+++ utils.py
@@ -734,7 +734,7 @@ async def send_all(bot, userid, files, ident):
                             InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ", url=GRP_LNK),
                             InlineKeyboardButton("Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url=CHNL_LNK),
                         ],
-                        [InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")],
+                        [InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="telegram.me/mpbotzsupport_bot")],
                     ]
                 ),
             )
@@ -890,7 +890,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                         InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                         InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                     ],[
-                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Kgashok04")
+                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/mpbotzsupport_bot")
                         ]
                     ]
                 )
